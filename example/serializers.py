6e972acc9744c285f3a9512a7d2f67355ef6978f
Santiago Fulladoza <santiagofulladoza@gmail.com>
2016-07-06 16:30:23 -0300

    Multiple nested grouping added

--- example/serializers.py
+++ example/serializers.py
@@ -1,9 +1,15 @@
  # -*- coding: utf-8 -*-
 from rest_framework import serializers
-from example.models import Citizen
+from example.models import Citizen, CitizenExt
 
 class CitizenSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Citizen
         fields = ('name', 'age', 'city')
+
+class CitizenExtSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CitizenExt
+        fields = ('name', 'age', 'gender','country','city')
--- example/views.py
+++ example/views.py
@@ -2,8 +2,8 @@
 
 from django.shortcuts import render
 from nestgrouplists.views import *
-from example.serializers import CitizenSerializer
-from example.models import Citizen
+from example.serializers import CitizenSerializer, CitizenExtSerializer
+from example.models import Citizen, CitizenExt
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -26,10 +26,33 @@ def ListGrouped(request):
     ## 3: model over wich the request is made
     ## 4: modelserializer for the model
     listgrouped = simplegrouping('city', aggFields, Citizen, CitizenSerializer)
-    print(listgrouped)
+    return Response(listgrouped)
+
+# Method using nested-grouped-listing with annotations
+@api_view(['GET'])
+def NestListGrouped(request):
+    aggFields = [
+        {'function': 'Count', 'field': 'name'},
+        {'function': 'Sum', 'field': 'age'},
+        {'function': 'Avg', 'field': 'age'}
+    ]
+
+    # NESTED MULTIPLE GROUPING - Params
+    ## 1: fields to group by
+    ## 2: list of aggregations, where each element in the list is in the format {'function':'Func', 'field': 'Field'}
+        # Func is one of the followings: 'Sum', 'Avg', 'Max', 'Min', 'Count', 'StdDev', 'Variance'
+        # Field is the field of your model over which the function is applied
+    ## 3: model over wich the request is made
+    ## 4: modelserializer for the model
+    listgrouped = nestedgrouping(['country','city'], aggFields, CitizenExt, CitizenExtSerializer)
     return Response(listgrouped)
 
 # Class using standar listing
 class CitizenView(generics.ListCreateAPIView):
     queryset = Citizen.objects.all()
     serializer_class = CitizenSerializer
+
+# Class using standar listing
+class CitizenExtView(generics.ListCreateAPIView):
+    queryset = CitizenExt.objects.all()
+    serializer_class = CitizenExtSerializer
